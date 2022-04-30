9f537ad314badfc98e7458ac549a8ec49f80131a
Adeline <adeline.sire@gmail.com>
2022-04-30 08:08:24 +0200

    delete expired offers in saveDetails

--- les-jeudis/getDetailsUrls.js
+++ les-jeudis/getDetailsUrls.js
@@ -4,7 +4,6 @@ const getDetailsUrls = async () => {
 	try {
 		console.time('getDetailsUrls');
 		const jobs = await JobRaw.find({ description: null }).exec();
-		console.log('jobs.length getDetailsUrls : ', jobs.length);
 		const urls = jobs.map((job) => job.url);
 		console.log('urls.length getDetailsUrls : ', urls.length);
 		console.timeEnd('getDetailsUrls');
--- les-jeudis/saveDetails.js
+++ les-jeudis/saveDetails.js
@@ -5,9 +5,13 @@ const saveDetails = async (details) => {
 		console.log('details.length in saveDetails : ', details.length);
 		console.time('saveDetails');
 		for (let detail of details) {
-			const job = await JobRaw.findOne({ url: detail.url });
-			job.description = detail.description;
-			await job.save();
+			if (detail.description) {
+				const job = await JobRaw.findOne({ url: detail.url });
+				job.description = detail.description;
+				await job.save();
+			} else {
+				await Job.deleteOne({ url: detail.url });
+			}
 		}
 		console.timeEnd('saveDetails');
 	} catch (error) {
